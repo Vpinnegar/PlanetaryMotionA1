8752ee7b3348858c535377c8eb3c7b6d779bf5d4
Vpinnegar <70932660+Vpinnegar@users.noreply.github.com>
2024-11-10 23:35:03 -0500

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -2,15 +2,15 @@
 """
 Planetary Motion Question 3
 
-Description: This module...
+Description: This module is directly related to the orbital elements of eccentricity
+true anomaly, and eccentric anomaly. The purpose of this module is to take in either true anomaly
+and eccentricity or eccentric anomaly and eccetricity and find the final parameter.
 
-Example:
-        
-    
 Attributes:
     Inputs: 
-        Eccentricity e: radians
-        True anomaly f: radians
+        Eccentricity e: float 
+        True anomaly f: float units: radians
+        
     
     
 TODO:
@@ -19,18 +19,44 @@ TODO:
 
 """
 import math
-import numpy
+import numpy as np
 
 def ta_to_eccentric_a(e,f):
     """Parameters:
-            f: True anomaly in Radians
-            e: Eccentricity in Radians """
+        Input:
+            f: True anomaly float Units: Radians
+            e: Eccentricity float 
+        Output:
+            E: Eccentric Anomaly float Units: Radians
+            """
     if f > 2*np.pi:
         raise ValueError(f"The value {f} exceeds the threshold for the True anomaly. This must be within range 0 < f < 2pi.")
+    if not isinstance(e, (int, float)):
+        raise TypeError(f"Error: The Eccentricity ({e}) is not numeric")
+    if not isinstance(f, (int, float)):
+        raise TypeError(f"Error: The True Anomaly ({f}) is not numeric")
     return 2 * math.atan(math.sqrt((1 - e) / (1 + e)) * math.tan(f / 2))
+
+## Test
+e = 0.1 
+f = 2
+print(ta_to_eccentric_a(e,f))
+
         
 def eccentricity_to_ta(e, E):
-    """ Paramters:
-            e: Eccentricity in Radians
-            E: Eccentric Anomaly in radians"""
+    """Parameters:
+        Input:
+            E: Eccentric anomaly float Units: Radians
+            e: Eccentricity float 
+        Output:
+            f: True Anomaly float Units: Radians
+            """
+    if not isinstance(e, (int, float)):
+        raise TypeError(f"Error: The Eccentricity ({e}) is not numeric")
+    if not isinstance(E, (int, float)):
+        raise TypeError(f"Error: The Eccentric Anomaly ({E}) is not numeric")
     return 2 * math.atan(math.sqrt((1 + e) / (1 - e)) * math.tan(E / 2))
+## Test
+e = 0.1 
+E = 1.90696691269126843427
+print(eccentricity_to_ta(e,E))
